1a66d64120612ee599b02df4404c1f996734f632
태양 허 <oceanshape700@gmail.com>
2021-05-07 15:30:36 +0900

    feat: Add update() and draw() from main loop

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cassert>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -11,89 +13,52 @@ float gPositionX = 0.5f;
 float gPositionY = 0.5f;
 float moveX = 0.0f;
 float moveY = 0.0f;
+const float delta = 0.05f;
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, GLFW_TRUE);
-	}
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)	glfwSetWindowShouldClose(window, GLFW_TRUE);
 
-	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
-	{
-		moveX = -0.1f;
-	}
-	if (key == GLFW_KEY_LEFT && action == GLFW_RELEASE)
-	{
-		moveX = 0.0f;
-	}
-
-	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
-	{
-		moveX = 0.1f;
-	}
-	if (key == GLFW_KEY_RIGHT && action == GLFW_RELEASE)
-	{
-		moveX = 0.0f;
-	}
-
-
-	if (key == GLFW_KEY_UP && action == GLFW_PRESS)
-	{
-		moveY = 0.1f;
-	}
-	if (key == GLFW_KEY_UP && action == GLFW_RELEASE)
-	{
-		moveY = 0.0f;
-	}
-
-	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
-	{
-		moveY = -0.1f;
-	}
-	if (key == GLFW_KEY_DOWN && action == GLFW_RELEASE)
-	{
-		moveY = 0.0f;
-	}
+	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS) moveX = -delta;
+	if (key == GLFW_KEY_LEFT && action == GLFW_RELEASE)	moveX = 0.0f;
+	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS) moveX = delta;
+	if (key == GLFW_KEY_RIGHT && action == GLFW_RELEASE) moveX = 0.0f;
+	if (key == GLFW_KEY_UP && action == GLFW_PRESS) moveY = delta;
+	if (key == GLFW_KEY_UP && action == GLFW_RELEASE) moveY = 0.0f;
+	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS) moveY = -delta;
+	if (key == GLFW_KEY_DOWN && action == GLFW_RELEASE) moveY = 0.0f;
 }
 
-int main() {
-	GLFWwindow* glfwWindow = nullptr;
+bool init(GLFWwindow** pGlfwWindow)
+{
 	int width = DEFAULT_WIDTH;
 	int height = DEFAULT_HEIGHT;
 	string title = "GELDA";
 
-	if (!glfwInit())
-	{
-		return -1;
-	}
 
-	glfwWindow = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+	if (!glfwInit()) return false;
 
-	glfwMakeContextCurrent(glfwWindow);
-	glfwSetKeyCallback(glfwWindow, keyCallback);
-	glfwGetFramebufferSize(glfwWindow, &width, &height);
+	*pGlfwWindow = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+
+	glfwMakeContextCurrent(*pGlfwWindow);
+	glfwSetKeyCallback(*pGlfwWindow, keyCallback);
+	glfwGetFramebufferSize(*pGlfwWindow, &width, &height);
 
 	const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-	glfwSetWindowPos(glfwWindow, (mode->width - width) / 2, (mode->height - height) / 2);
+	glfwSetWindowPos(*pGlfwWindow, (mode->width - width) / 2, (mode->height - height) / 2);
 
-	if (!glfwWindow)
-	{
-		return -1;
-	}
+	if (!*pGlfwWindow) return false;
 
 
 	// Init GLEW
 	glewExperimental = true;
-	if (glewInit() != GLEW_OK)
-	{
-		return -1;
-	}
+	if (glewInit() != GLEW_OK) return false;
 
-	glfwSwapInterval(1); // set 60fps
 
-	const float aspect_ratio = (float)width / (float)height;
+	glfwSwapInterval(1); // 60fps
+
 	glViewport(0, 0, width, height);
+	const float aspect_ratio = (float)width / (float)height;
 	glOrtho(-aspect_ratio, aspect_ratio, -1.0, 1.0, -1.0, 1.0);
 
 	std::cout << "Display width = " << width << std::endl
@@ -101,33 +66,47 @@ int main() {
 		<< "Aspect ratio = " << aspect_ratio << std::endl;
 	std::cout << "Press Escape key to exit" << std::endl;
 
+	return true;
+}
 
-	while (!glfwWindowShouldClose(glfwWindow))
+void update()
+{
+	gPositionX += moveX;
+	gPositionY += moveY;
+}
+
+void draw()
+{
+	glClearColor(static_cast<float>(100) / 255.0f, static_cast<float>(149) / 255.0f, static_cast<float>(237) / 255.0f, 1);// background color
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+
+	glPushMatrix();
+
 	{
-		glfwMakeContextCurrent(glfwWindow);
-		// (100, 149, 237) is background color
-		glClearColor(static_cast<float>(100) / 255.0f, static_cast<float>(149) / 255.0f, static_cast<float>(237) / 255.0f, 1);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-
-		gPositionX += moveX;
-		gPositionY += moveY;
-
-
-		// rendering loop
-		glMatrixMode(GL_MODELVIEW);
-		glPushMatrix();
-
-		glColor3f(1, 1, 1);
+		glColor3f(0, 0, 0);
 		glBegin(GL_POLYGON);
 		glVertex2f(gPositionX, gPositionY);
 		glVertex2f(gPositionX - 1.0f, gPositionY);
 		glVertex2f(gPositionX - 1.0f, gPositionY - 1.0f);
 		glVertex2f(gPositionX, gPositionY - 1.0f);
 		glEnd();
+	}
 
-		glPopMatrix();
-		glFlush();
+	glPopMatrix();
+
+	glFlush();
+}
+
+int main() {
+	GLFWwindow* glfwWindow = nullptr;
+
+	init(&glfwWindow);
+
+	while (!glfwWindowShouldClose(glfwWindow))
+	{
+		update();
+		draw();
 
 		glfwSwapBuffers(glfwWindow);
 		glfwPollEvents();
